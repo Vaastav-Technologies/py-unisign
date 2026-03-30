ebd6e303186728c03ec66610d9b69e4df8376371
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2026-03-30 22:16:36 +0530

    Actual initial commit

---
+++ src/unisign/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+Unifying various python signing implementations
+"""
---
+++ tests/test_unisign.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+Simple initial unisign test.
+"""
+
+
+def test_simple():
+    assert 1 == 1
